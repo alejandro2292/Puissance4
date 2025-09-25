74b05ed3f1f00ff246bd68850ef5f27b2fe773dd
alejandro2292 <trashmailjunk0085@gmail.com>
2025-09-25 13:59:41 +0200

    ajout du systeme de vérification de victoire

--- main.ts
+++ main.ts
@@ -25,6 +25,49 @@ function poserPiece(colonne: number, joueur: string): boolean {
   return false;
 }
 
+// Vérification de victoire
+function verifierVictoire(joueur: string): boolean {
+  // Horizontal
+  for (let i = 0; i < LIGNES; i++) {
+    for (let j = 0; j <= COLONNES - 4; j++) {
+      if (grille[i][j] === joueur &&
+          grille[i][j+1] === joueur &&
+          grille[i][j+2] === joueur &&
+          grille[i][j+3] === joueur) return true;
+    }
+  }
+  // Vertical
+  for (let i = 0; i <= LIGNES - 4; i++) {
+    for (let j = 0; j < COLONNES; j++) {
+      if (grille[i][j] === joueur &&
+          grille[i+1][j] === joueur &&
+          grille[i+2][j] === joueur &&
+          grille[i+3][j] === joueur) return true;
+    }
+  }
+  // Diagonale descendante (\)
+  for (let i = 0; i <= LIGNES - 4; i++) {
+    for (let j = 0; j <= COLONNES - 4; j++) {
+      if (grille[i][j] === joueur &&
+          grille[i+1][j+1] === joueur &&
+          grille[i+2][j+2] === joueur &&
+          grille[i+3][j+3] === joueur) return true;
+    }
+  }
+  // Diagonale montante (/)
+  for (let i = 3; i < LIGNES; i++) {
+    for (let j = 0; j <= COLONNES - 4; j++) {
+      if (grille[i][j] === joueur &&
+          grille[i-1][j+1] === joueur &&
+          grille[i-2][j+2] === joueur &&
+          grille[i-3][j+3] === joueur) return true;
+    }
+  }
+
+  return false;
+}
+
+
 // Simulation des coups (solution temporaire pour testé la grille)
 let coups: number[] = [2, 3, 2, 1, 4, 2, 5]; 
 
